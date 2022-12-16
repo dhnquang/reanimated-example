e3afc6359022efbcde7f5fbb171ae399882df5b3
Quang <quang@Quangs-MacBook-Pro.local>
2022-12-16 09:37:20 +0700

    first commit

---
+++ react-native-gestures-and-animations/src/CircularSlider/boilerplate/HalfCircle.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { View } from "react-native";
+import Animated from "react-native-reanimated";
+
+interface HalfCircleProps {
+  color: Animated.Node<number> | string;
+  r: number;
+}
+
+const HalfCircle = ({ color, r }: HalfCircleProps) => {
+  return (
+    <View
+      style={{
+        width: r * 2,
+        height: r,
+        overflow: "hidden",
+      }}
+    >
+      <Animated.View
+        // @ts-expect-error
+        style={{
+          backgroundColor: color,
+          width: r * 2,
+          height: r * 2,
+          borderRadius: r,
+        }}
+      />
+    </View>
+  );
+};
+
+export default HalfCircle;
